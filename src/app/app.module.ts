8ec3723634db9fbc203d4ac84f3a5b005d92389f
Zura <zura.baramia98@outlook.com>
2022-12-23 16:44:34 +0400

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { SharedModule } from './shared/shared.module'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { RatingModule } from 'ng-starrating'
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        RatingModule,
         BrowserAnimationsModule,
         SharedModule,
+        NgbModule,
     ],
+    exports: [RatingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core'
 import { HeaderComponent } from '../core/header/header.component'
 import { MaterialModule } from './material.module'
+import { HomeComponent } from '../core/components/pages/home/home.component'
+import { CommonModule } from '@angular/common'
+import { RatingModule } from 'ng-starrating'
 
 @NgModule({
-    declarations: [HeaderComponent],
-    imports: [MaterialModule],
-    exports: [HeaderComponent],
+    declarations: [HeaderComponent, HomeComponent],
+    imports: [CommonModule, MaterialModule, RatingModule],
+    exports: [HeaderComponent, HomeComponent],
 })
 export class SharedModule {}
