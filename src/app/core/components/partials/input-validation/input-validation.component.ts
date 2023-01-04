30ffc11183fc5eb2d9218cebab6f208c79e6c7a0
zura <zura.baramia98@outook.com>
2023-01-04 15:52:01 +0400

    login page update

---
+++ src/app/core/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,47 @@
+import {
+    Component,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+} from '@angular/core'
+import { AbstractControl } from '@angular/forms'
+
+const VALIDATORS: any = {
+    required: 'Should not be empty',
+    email: 'Email is not valid',
+}
+@Component({
+    selector: 'app-input-validation',
+    templateUrl: './input-validation.component.html',
+    styleUrls: ['./input-validation.component.scss'],
+})
+export class InputValidationComponent implements OnInit, OnChanges {
+    @Input() control!: AbstractControl
+    @Input() showErors: boolean = true
+    errorMessage: string[] = []
+
+    constructor() {}
+    checkValidation() {
+        const error = this.control.errors
+        if (!error) {
+            this.errorMessage = []
+            return
+        }
+        const errorKey = Object.keys(error)
+        this.errorMessage = errorKey.map((key) => VALIDATORS[key])
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.checkValidation()
+    }
+
+    ngOnInit(): void {
+        this.control.statusChanges.subscribe(() => {
+            this.checkValidation()
+        })
+        this.control.valueChanges.subscribe(() => {
+            this.checkValidation()
+        })
+    }
+}
